cfa0e1ee9292750f24ba74176a939b0cf7fbee45
Matt Chadburn <matt.chadburn@guardian.co.uk>
2014-01-30 21:04:33 +0000

    Added cities to demo

--- examples/example.js
+++ examples/example.js
@@ -1,19 +1,33 @@
 var Nearest = require('../nearest');
 
 window.addEventListener('load', function () {
-  var coords = window.location.hash.substring(1).split(",")
-    , lat = coords[0] || 51.5349
-    , lon = coords[1] || -0.1219
-    , r = document.getElementById('reviews')
-    , results = new Nearest().find(reviews, { 
-        coords: [lat, lon],
-        limit: 10,
-        within: 3
-      }).forEach(function (review) {
-        var i = document.createElement('li');
-        i.innerHTML = ' <a href="http://www.theguardian.com/'+ review.path + '" target="_blank">'+review.restaurant +'</a>';
-        i.innerHTML += ', ' + review.postcode;
-        i.innerHTML += ' <small>' + review.distance + ' km</small> - reviewed by ' + review.reviewer;
-        r.appendChild(i);
-      });
+    var r = document.getElementById('reviews')
+      , results = function () {
+          var coords = window.location.hash.substring(1).split(",")
+            , lat = coords[0] || 51.5349
+            , lon = coords[1] || -0.1219;
+          while (r.hasChildNodes()) {
+                r.removeChild(r.lastChild);
+          }
+          new Nearest().find(reviews, { 
+          coords: [lat, lon],
+          limit: 10,
+          within: 10 
+        }).forEach(function (review) {
+          var i = document.createElement('li');
+          i.innerHTML = ' <a href="http://www.theguardian.com/'+ review.path + '" target="_blank">'+review.restaurant +'</a>';
+          i.innerHTML += ', ' + review.postcode;
+          i.innerHTML += ' <small>' + review.distance + ' km</small> - reviewed by ' + review.reviewer;
+          r.appendChild(i);
+        });
+      }
+
+    // go
+    
+    results();
+    
+    window.addEventListener("hashchange", function (e) {
+        results.call(this);
+    }, false);
+
 }, false);
